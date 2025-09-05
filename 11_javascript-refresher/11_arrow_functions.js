092b75b26cfe56f080ce4afbdc19ef7e4bf00c21
auhsojp <jashuapatenia15@gmail.com>
2025-09-06 02:44:30 +0800

    Add arrow functions activity

---
+++ 11_javascript-refresher/11_arrow_functions.js
@@ -0,0 +1,15 @@
+const greet = (name) => {
+  return "Hello, " + name;
+}
+
+const square = (n) => {
+  return n * n;
+}
+
+const sayHi = () => {
+  console.log("Hi!");
+}
+
+console.log(greet("joshua"));
+console.log(square(3));
+sayHi();
