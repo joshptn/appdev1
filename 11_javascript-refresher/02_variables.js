510f8ca70d0a0284a8b93a1d04eeee79779c4221
auhsojp <jashuapatenia15@gmail.com>
2025-09-06 00:44:50 +0800

    Add variables activity

---
+++ 11_javascript-refresher/02_variables.js
@@ -0,0 +1,23 @@
+const name = "joshua"
+const age = 22
+const isStudent = true
+
+console.log(name);
+console.log(age);
+console.log(isStudent);
+
+console.log(typeof name);
+console.log(typeof age);
+console.log(typeof isStudent);
+
+
+let x = 10
+let y = 5
+
+console.log(x + y);
+console.log(x - y);
+console.log(x * y);
+console.log(x / y);
+
+console.log("5" == 5);   
+console.log("5" === 5);  
