6c30a08c8a79c1fc6fc43288106177b7cb2a658a
auhsojp <jashuapatenia15@gmail.com>
2025-09-06 02:41:23 +0800

    Add let and const activity

---
+++ 11_javascript-refresher/10_let_const.js
@@ -0,0 +1,18 @@
+var name = "joshua";
+var age = 21;
+name = "shua";
+age = 22;
+
+let letName = "joshua";
+let letAge = 21;
+letName = "shua";
+letAge = 22;
+
+// const constName = "joshua";
+// const constAge = 21;
+// constName = "shua";
+// constAge = 22;
+// error to reassignment
+
+console.log(name);
+console.log(age);
