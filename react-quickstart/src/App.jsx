8cd25d969fd20202c4481eb383a8addc134a0e56
auhsojp <jashuapatenia15@gmail.com>
2025-09-17 11:13:30 +0800

    Done writing markup with JSX

--- react-quickstart/src/App.jsx
+++ react-quickstart/src/App.jsx
@@ -15,15 +15,24 @@ function MyApp() {
   );
 }
 
+function AboutPage() {
+  return (
+    <>
+      <h1>About</h1>
+      <p>Hello there.<br />How do you do?</p>
+    </>
+  );
+}
+
 
 function App() {
   return (
     <>
-
+      <AboutPage />
     </>
   )
 }
 
-export default MyApp
+export default App
 
 
